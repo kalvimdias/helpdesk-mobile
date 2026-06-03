26016588e9cb751e4c2021e7e82e573d6c9f7c78
Kalvim Dias <kalvimdias@hotmail.com>
2026-06-03 14:34:43 -0300

    feat: cria estrutura base com riverpod e gorouter

---
+++ flutter_app/lib/core/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+    useMaterial3: true,
+  );
+}
